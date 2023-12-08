b6a1654e1c049ceccb1806b65056cf0646e4cc2a
faiinrd <nopradakrit@gmail.com>
2023-12-08 16:39:05 +0700

    lab7 completed

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+ring ID,GEAR;
+    // cout << "Fahsai: I think you are an Engineering student. What is your student ID?\n";
+    // cout << name << ": ";
+    // cin >> ID;
+    // ID=ID[0]+ID[1];
+    // GEAR=ID-12;
+    // // char ID = cin.get();
+    // cout << "Fahsai: I think you may be GEAR " << GEAR << ". I have a free movie ticket for you.\n";
